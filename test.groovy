11c159b5d49fda9617fd184b396f805669a44fe0
AjKarthik5 <ajaykarthik5601@gmail.com>
2025-05-23 00:43:11 +1000

    Initial commit

---
+++ test.groovy
@@ -0,0 +1,47 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building the code using Maven'
+            }
+        }
+
+        stage('Unit & Integration Tests') {
+            steps {
+                echo 'Running tests using JUnit and Mocha'
+            }
+        }
+
+        stage('Code Analysis') {
+            steps {
+                echo 'Analyzing code quality using SonarQube'
+            }
+        }
+
+        stage('Security Scan') {
+            steps {
+                echo 'Scanning for vulnerabilities with Snyk'
+            }
+        }
+
+        stage('Deploy to Staging') {
+            steps {
+                echo 'Deploying to staging environment using Docker'
+            }
+        }
+
+        stage('Integration Tests on Staging') {
+            steps {
+                echo 'Running end-to-end tests on staging'
+            }
+        }
+
+        stage('Deploy to Production') {
+            steps {
+                echo 'Deploying to production using AWS CLI'
+            }
+        }
+    }
+}
